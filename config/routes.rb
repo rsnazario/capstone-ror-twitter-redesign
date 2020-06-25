757d6ddab4e9078887792ee239775b9cf724d856
rsnazario <rafael.s.nazario@gmail.com>
2020-06-25 15:51:23 -0300

    Setup Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create, :index, :show]
+  resources :opinions, only: [:new, :create, :index]
+  resources :followings, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
