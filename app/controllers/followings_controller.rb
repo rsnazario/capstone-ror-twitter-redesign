8556d2df89feaf85e138700ce95fc684123544e7
rsnazario <rafael.s.nazario@gmail.com>
2020-06-29 14:19:03 -0300

    Added view functionality create#following

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -2,8 +2,15 @@ class FollowingsController < ApplicationController
   def new; end
 
   def create
-    @follow = current_user.follows.build(followed_id: params[:id])
-    @follow.save
+    @follow = Following.create(friendship_params)
+    # @follow = current_user.follows.build(followed_id: params[:id])
+    # @follow.save
     redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def friendship_params
+    params.permit(:follower_id, :followed_id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,9 @@
 class User < ApplicationRecord
   has_many :thoughts, class_name: 'Opinion', foreign_key: 'author_id'
 
-  def yet_to_follow 
-    yet = User.all
-    exclude_list = [current_user.id]
-    current_user.ppl_following.each do |following| 
-      exclude_list << following.followed_id
-    end
-    yet
-  end
-
   has_many :follows, foreign_key: 'follower_id', class_name: 'Following'
   has_many :ppl_following, through: :follows, source: 'followed'
 
-  scope :yet, -> { where('followed_id > ?', 1) }
-
-
   has_many :followers, foreign_key: 'followed_id', class_name: 'Following'
-  has_many :ppl_followers, through: :followers, source: 'followed'
+  has_many :ppl_followers, through: :followers, source: 'follower'
 end
