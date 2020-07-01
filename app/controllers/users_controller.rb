13658eba5f81ed8abc1355735fa1b7d67bac5d06
rsnazario <rafael.s.nazario@gmail.com>
2020-07-01 18:33:47 -0300

    Added Callback

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user, only: [:index, :show]
+
   def new
     @user = User.new
   end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,4 +3,15 @@ module SessionsHelper
     current_user = User.find_by(id: cookies[:current_user_id])
     current_user
   end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def authenticate_user
+    unless logged_in?
+      flash[:notice] = 'Please Log In!'
+      redirect_to root_path
+    end
+  end
 end
