fa25745ce1c4508b3802181d1333f3b76ad46fce
rsnazario <rafael.s.nazario@gmail.com>
2020-07-06 15:10:24 -0300

    Added create#opinion route to create from show-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :index, :show]
   resources :opinions, only: [:new, :create, :index]
+  post 'create_opinion', to: 'opinions#create'
+  
   resources :followings, only: [:new, :create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
+
+
+
   root 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
